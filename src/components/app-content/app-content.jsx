702711bdd9255215b7a8d8ba7762985944091cd4
Artem Mazin <minima1s@yandex.ru>
2023-11-16 22:43:51 +0300

    refactor: выделить компонент Navigation

---
+++ src/components/app-content/app-content.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function AppContent() {
+  return <div>a</div>;
+}
+
+export default AppContent;
--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import styles from './app.module.css';
 import AppHeader from './app-header/app-header';
+import AppContent from './app-content/app-content';
 
 export default function App() {
   return (
     <div className={styles.app}>
       <AppHeader />
+      <AppContent />
     </div>
   );
 }
