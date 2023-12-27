de1a52040ecb4a783b4900b998053a66f4069079
ArtemMazin <minima1s@yandex.ru>
2023-12-27 13:52:17 +0300

    style: адаптировать меню с конструктором под мобильную версию

--- src/components/burger-constructor/burger-components/bun/bun.jsx
+++ src/components/burger-constructor/burger-components/bun/bun.jsx
@@ -3,10 +3,13 @@ import PropTypes from 'prop-types';
 import { ConstructorElement } from '@ya.praktikum/react-developer-burger-ui-components';
 import styles from './bun.module.css';
 import ingredientPropTypes from '@/utils/prop-types';
+import { useResize } from '@/hooks/useResize';
 
 export default function Bun({ bun, type, text }) {
+  const { isMobile } = useResize();
+
   return (
-    <div className={`${styles.component} ml-8 mr-3`}>
+    <div className={`${styles.component} ${isMobile ? 'ml-6' : 'ml-8'} mr-3`}>
       <ConstructorElement
         type={type}
         isLocked={true}
--- src/components/burger-constructor/burger-constructor.jsx
+++ src/components/burger-constructor/burger-constructor.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import BurgerComponents from './burger-components/burger-components';
 import BurgerOrder from './burger-order/burger-order';
-import styles from './burger-constructor.module.css';
+import { useResize } from '@/hooks/useResize';
 
 function BurgerConstructor() {
+  const { isMobile } = useResize();
+
   return (
-    <section className={`${styles.container} pt-25 pl-4`}>
+    <section className={`${isMobile ? 'pl-2' : 'pt-25 pl-4'}`}>
       <BurgerComponents />
       <BurgerOrder />
     </section>
